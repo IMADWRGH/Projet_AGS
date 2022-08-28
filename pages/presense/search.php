f30a20ee8cab3d2d2448f546cf92c9af94a5aa2b
Bio <amasin76@gmail.com>
2022-08-28 23:31:38 +0100

    adjust search query to get last intership

--- pages/presense/search.php
+++ pages/presense/search.php
@@ -6,7 +6,13 @@ if (isset($_POST['search'])) {
 
     $search = mysqli_real_escape_string($con, $_POST['search']);
 
-    $query = "SELECT * FROM stagiaire WHERE NOM LIKE '$search%'";
+    $query = "SELECT `sr`.*, `st`.`ID_STAGE`
+                FROM `stagiaire` AS `sr`
+                LEFT JOIN `stage` AS `st` ON `sr`.`ID_STAGE` = `st`.`ID_STAGE`
+                WHERE sr.`NOM` LIKE '$search%' OR sr.`PRENOM` LIKE '$search%' OR st.ID_STAGE = '$search'
+                ORDER BY ABS( DATEDIFF( st.DATE_D, CURRENT_DATE ) ) 
+                LIMIT 1
+    ";
     $query_run = mysqli_query($con, $query);
 
     if (mysqli_num_rows($query_run) > 0) {
@@ -14,7 +20,7 @@ if (isset($_POST['search'])) {
         $stagiaire = mysqli_fetch_array($query_run);
         $res = array();
 
-        $res[] = array("value" => $stagiaire['ID_STAGIAIRE'], "label" => $stagiaire['NOM'] . " " . $stagiaire['PRENOM']);
+        $res[] = array("value" => $stagiaire['ID_STAGE'], "label" => $stagiaire['NOM'] . " " . $stagiaire['PRENOM']);
         echo json_encode($res);
         return false;
     } else {
