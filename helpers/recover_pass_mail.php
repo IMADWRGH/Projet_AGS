193bc3372a7651fb30fa8b977b93d62c5ce8d521
Imad Asserne <imad.asserne@gmail.com>
2022-08-29 21:35:33 +0100

    new page

---
+++ helpers/recover_pass_mail.php
@@ -0,0 +1,20 @@
+ <?php
+session_start();
+include_once "./condb.php";
+if(isset($_POST["reset"])){
+    $mail=$_POST["mail"];
+    $username=$_POST["username"];
+    $query="SELECT * FROM UTILISATEUR WHERE email='$mail' and username='$username'";
+    $result = mysqli_query($con, $query);
+    $count=mysqli_num_rows($result);
+    if($count==0){
+    
+        header("location: ../forget_password.php?error=Incorrect E-mail");
+        die;
+    }else{
+       
+         header("location: ../Reset_password.php");
+            die;
+    }
+} 
+ 
