47fd216cb52ba524cd72ddcf8ee1985af4b24fa0
Bio <amasin76@gmail.com>
2022-08-29 23:54:06 +0100

    links table with 'ID_STAGE'

--- abhoer_db.sql
+++ abhoer_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 26, 2022 at 05:13 AM
+-- Generation Time: Aug 30, 2022 at 12:31 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -32,7 +32,8 @@ CREATE TABLE `contination` (
   `TACHES` varchar(100) NOT NULL,
   `ABSENCES` smallint(6) DEFAULT NULL,
   `TEMPS_DEPLOYE` smallint(6) DEFAULT NULL,
-  `EQUIPEMENT` int(11) NOT NULL
+  `EQUIPEMENT` int(11) NOT NULL,
+  `ID_STAGE` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -50,7 +51,8 @@ CREATE TABLE `dossier` (
   `COPY_CIN` int(11) NOT NULL,
   `DATE_DEPOSE` date NOT NULL,
   `STATUT` char(15) NOT NULL,
-  `AUTRE_FICHERS` int(11) NOT NULL
+  `AUTRE_FICHERS` int(11) NOT NULL,
+  `ID_STAGE` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -74,7 +76,8 @@ CREATE TABLE `evaluation` (
   `E11` char(10) NOT NULL,
   `E12` char(10) NOT NULL,
   `E13` char(10) NOT NULL,
-  `COMMONTAIRE` char(255) NOT NULL
+  `COMMONTAIRE` char(255) NOT NULL,
+  `ID_STAGE` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -86,31 +89,40 @@ CREATE TABLE `evaluation` (
 CREATE TABLE `filiere` (
   `ID_FILIERE` char(10) NOT NULL,
   `NOM_FILIERE` char(50) NOT NULL,
-  `CHEF_FILIERE` char(50) NOT NULL
+  `CHEF_FILIERE` char(50) NOT NULL,
+  `ID_STAGE` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hr_presence`
+-- Table structure for table `presence`
 --
 
-CREATE TABLE `hr_presence` (
+CREATE TABLE `presence` (
   `ID_PRESENCE` char(10) NOT NULL,
   `DATE` date NOT NULL,
-  `HR_ENTRE_M` time NOT NULL,
-  `HR_SORTIE_M` time NOT NULL,
-  `HR_ENTRE_A` time NOT NULL,
-  `HR_SORTIE_A` time NOT NULL,
-  `OBSERVATION` char(255) NOT NULL
+  `HR_ENTRE_M` time DEFAULT NULL,
+  `HR_SORTIE_M` time DEFAULT NULL,
+  `HR_ENTRE_A` time DEFAULT NULL,
+  `HR_SORTIE_A` time DEFAULT NULL,
+  `OBSERVATION` char(255) NOT NULL,
+  `ID_STAGE` char(10) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `hr_presence`
+-- Dumping data for table `presence`
 --
 
-INSERT INTO `hr_presence` (`ID_PRESENCE`, `DATE`, `HR_ENTRE_M`, `HR_SORTIE_M`, `HR_ENTRE_A`, `HR_SORTIE_A`, `OBSERVATION`) VALUES
-('P1', '2022-08-25', '08:35:15', '12:59:59', '14:00:00', '16:12:01', 'tttt');
+INSERT INTO `presence` (`ID_PRESENCE`, `DATE`, `HR_ENTRE_M`, `HR_SORTIE_M`, `HR_ENTRE_A`, `HR_SORTIE_A`, `OBSERVATION`, `ID_STAGE`, `created_at`, `updated_at`) VALUES
+('P10', '2022-08-28', '00:00:00', '00:00:00', '00:00:00', '00:00:00', '', 'T1', '2022-08-29 02:47:18', '2022-08-29 20:31:22'),
+('P11', '2022-08-29', '17:01:00', '00:00:00', '00:00:00', '00:00:00', '', 'T1', '2022-08-29 16:02:00', '2022-08-29 17:02:00'),
+('P12', '2022-08-29', '17:03:00', '00:00:00', '00:00:00', '00:00:00', '', 'T1', '2022-08-29 16:03:42', '2022-08-29 17:03:42'),
+('P13', '2022-08-29', '19:41:00', '00:00:00', '00:00:00', '00:00:00', '', 'T1', '2022-08-29 18:41:49', '2022-08-29 19:41:49'),
+('P14', '2022-08-29', '19:44:00', '00:00:00', '00:00:00', '00:00:00', '', 'T1', '2022-08-29 18:44:16', '2022-08-29 19:44:16'),
+('P15', '2022-08-29', '19:50:00', '00:00:00', '00:00:00', '00:00:00', '', 'T2', '2022-08-29 18:47:14', '2022-08-29 19:47:14');
 
 -- --------------------------------------------------------
 
@@ -119,25 +131,20 @@ INSERT INTO `hr_presence` (`ID_PRESENCE`, `DATE`, `HR_ENTRE_M`, `HR_SORTIE_M`, `
 --
 
 CREATE TABLE `stage` (
-  `ID_STAGE` int(10) NOT NULL,
+  `ID_STAGE` char(10) NOT NULL,
   `DATE_D` date NOT NULL,
   `DATE_F` date NOT NULL,
   `ENCADRENT` char(50) NOT NULL,
-  `DEP` char(50) NOT NULL,
-  `ID_STAGIAIRE` char(10) NOT NULL,
-  `ID_PRESENCE` char(10) NOT NULL,
-  `ID_FILIERE` char(10) NOT NULL,
-  `ID_EVALUATION` char(10) NOT NULL,
-  `ID_DOSSIER` char(10) NOT NULL,
-  `ID_CONTINATION` char(10) NOT NULL
+  `DEP` char(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `stage`
 --
 
-INSERT INTO `stage` (`ID_STAGE`, `DATE_D`, `DATE_F`, `ENCADRENT`, `DEP`, `ID_STAGIAIRE`, `ID_PRESENCE`, `ID_FILIERE`, `ID_EVALUATION`, `ID_DOSSIER`, `ID_CONTINATION`) VALUES
-(1, '2022-08-01', '2022-08-31', 'Khaled', 'IT', 'ST1', 'P1', 'F1', 'E1', 'D1', 'C1');
+INSERT INTO `stage` (`ID_STAGE`, `DATE_D`, `DATE_F`, `ENCADRENT`, `DEP`) VALUES
+('T1', '2022-08-01', '2022-08-31', 'Khaled', 'IT'),
+('T2', '2022-08-01', '2022-09-30', 'achraf', 'DPH');
 
 -- --------------------------------------------------------
 
@@ -156,15 +163,17 @@ CREATE TABLE `stagiaire` (
   `VILLE` varchar(30) NOT NULL,
   `ADRESSE` varchar(100) NOT NULL,
   `ETABLISSEMENT` char(100) NOT NULL,
-  `NIVEAU` char(50) NOT NULL
+  `NIVEAU` char(50) NOT NULL,
+  `ID_STAGE` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `stagiaire`
 --
 
-INSERT INTO `stagiaire` (`ID_STAGIAIRE`, `CIN`, `NOM`, `PRENOM`, `SEXE`, `TEL`, `EMAIL`, `VILLE`, `ADRESSE`, `ETABLISSEMENT`, `NIVEAU`) VALUES
-('ST1', 'IA111222', 'MBARKI', 'BIO', 'M', '0611223344', 'test@test.com', 'BENI MELLAL', 'TAGZIRT', 'ECOSIG', 'BAC+2');
+INSERT INTO `stagiaire` (`ID_STAGIAIRE`, `CIN`, `NOM`, `PRENOM`, `SEXE`, `TEL`, `EMAIL`, `VILLE`, `ADRESSE`, `ETABLISSEMENT`, `NIVEAU`, `ID_STAGE`) VALUES
+('S1', 'IA111222', 'MBARKI', 'BIO', 'M', '0611223344', 'test@test.com', 'BENI MELLAL', 'TAGZIRT', 'ECOSIG', 'BAC+2', 'T1'),
+('S2', 'I333222', 'TAFU', 'OMAR', 'F', '0622334455', 'imar@omar.com', 'AGADIR', 'AOURIR', 'ISTA', 'BAC+2', 'T2');
 
 -- --------------------------------------------------------
 
@@ -197,65 +206,55 @@ INSERT INTO `utilisateur` (`USER_ID`, `USERNAME`, `PWD`, `EMAIL`, `ROLE`, `ETAT`
 -- Indexes for table `contination`
 --
 ALTER TABLE `contination`
-  ADD PRIMARY KEY (`ID_CONTINATION`);
+  ADD PRIMARY KEY (`ID_CONTINATION`),
+  ADD KEY `ID_STAGE` (`ID_STAGE`);
 
 --
 -- Indexes for table `dossier`
 --
 ALTER TABLE `dossier`
-  ADD PRIMARY KEY (`ID_DOSSIER`);
+  ADD PRIMARY KEY (`ID_DOSSIER`),
+  ADD KEY `ID_STAGE` (`ID_STAGE`);
 
 --
 -- Indexes for table `evaluation`
 --
 ALTER TABLE `evaluation`
-  ADD PRIMARY KEY (`ID_EVALUATION`);
+  ADD PRIMARY KEY (`ID_EVALUATION`),
+  ADD KEY `ID_STAGE` (`ID_STAGE`);
 
 --
 -- Indexes for table `filiere`
 --
 ALTER TABLE `filiere`
-  ADD PRIMARY KEY (`ID_FILIERE`);
+  ADD PRIMARY KEY (`ID_FILIERE`),
+  ADD KEY `ID_STAGE` (`ID_STAGE`);
 
 --
--- Indexes for table `hr_presence`
+-- Indexes for table `presence`
 --
-ALTER TABLE `hr_presence`
-  ADD PRIMARY KEY (`ID_PRESENCE`);
+ALTER TABLE `presence`
+  ADD PRIMARY KEY (`ID_PRESENCE`),
+  ADD KEY `ID_STAGE` (`ID_STAGE`);
 
 --
 -- Indexes for table `stage`
 --
 ALTER TABLE `stage`
-  ADD PRIMARY KEY (`ID_STAGE`),
-  ADD KEY `ID_STAGIAIRE` (`ID_STAGIAIRE`),
-  ADD KEY `ID_PRESENSE` (`ID_PRESENCE`),
-  ADD KEY `ID_FILIERE` (`ID_FILIERE`),
-  ADD KEY `ID_EVALUATION` (`ID_EVALUATION`),
-  ADD KEY `ID_DOSSIER` (`ID_DOSSIER`),
-  ADD KEY `ID_CONTINATION` (`ID_CONTINATION`);
+  ADD PRIMARY KEY (`ID_STAGE`);
 
 --
 -- Indexes for table `stagiaire`
 --
 ALTER TABLE `stagiaire`
-  ADD PRIMARY KEY (`ID_STAGIAIRE`);
+  ADD PRIMARY KEY (`ID_STAGIAIRE`),
+  ADD KEY `ID_STAGE` (`ID_STAGE`);
 
 --
 -- Indexes for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`USER_ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `stage`
---
-ALTER TABLE `stage`
-  MODIFY `ID_STAGE` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -265,37 +264,37 @@ ALTER TABLE `stage`
 -- Constraints for table `contination`
 --
 ALTER TABLE `contination`
-  ADD CONSTRAINT `contination_ibfk_1` FOREIGN KEY (`ID_CONTINATION`) REFERENCES `stage` (`ID_CONTINATION`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `contination_ibfk_1` FOREIGN KEY (`ID_STAGE`) REFERENCES `stage` (`ID_STAGE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `dossier`
 --
 ALTER TABLE `dossier`
-  ADD CONSTRAINT `dossier_ibfk_1` FOREIGN KEY (`ID_DOSSIER`) REFERENCES `stage` (`ID_DOSSIER`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `dossier_ibfk_1` FOREIGN KEY (`ID_STAGE`) REFERENCES `stage` (`ID_STAGE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `evaluation`
 --
 ALTER TABLE `evaluation`
-  ADD CONSTRAINT `evaluation_ibfk_1` FOREIGN KEY (`ID_EVALUATION`) REFERENCES `stage` (`ID_EVALUATION`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `evaluation_ibfk_1` FOREIGN KEY (`ID_STAGE`) REFERENCES `stage` (`ID_STAGE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `filiere`
 --
 ALTER TABLE `filiere`
-  ADD CONSTRAINT `filiere_ibfk_1` FOREIGN KEY (`ID_FILIERE`) REFERENCES `stage` (`ID_FILIERE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `filiere_ibfk_1` FOREIGN KEY (`ID_STAGE`) REFERENCES `stage` (`ID_STAGE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `hr_presence`
+-- Constraints for table `presence`
 --
-ALTER TABLE `hr_presence`
-  ADD CONSTRAINT `hr_presence_ibfk_1` FOREIGN KEY (`ID_PRESENCE`) REFERENCES `stage` (`ID_PRESENCE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `presence`
+  ADD CONSTRAINT `presence_ibfk_1` FOREIGN KEY (`ID_STAGE`) REFERENCES `stage` (`ID_STAGE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `stagiaire`
 --
 ALTER TABLE `stagiaire`
-  ADD CONSTRAINT `stagiaire_ibfk_1` FOREIGN KEY (`ID_STAGIAIRE`) REFERENCES `stage` (`ID_STAGIAIRE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `stagiaire_ibfk_1` FOREIGN KEY (`ID_STAGE`) REFERENCES `stage` (`ID_STAGE`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
