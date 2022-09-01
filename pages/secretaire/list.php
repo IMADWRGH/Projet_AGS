16797b929e0fc14e0b624f2471b05759c9988dc0
Bio <amasin76@gmail.com>
2022-09-01 06:14:33 +0100

    fix lniks of logout

--- pages/secretaire/list.php
+++ pages/secretaire/list.php
@@ -187,7 +187,7 @@ if (!isset($_SESSION['role'])) {
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="../../helpers/logout.php">Logout</a>
                 </div>
             </div>
         </div>
