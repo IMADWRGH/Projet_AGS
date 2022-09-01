eb10052783cf8111a4c9861d3397e76b667d6559
Bio <amasin76@gmail.com>
2022-09-01 02:32:41 +0100

    insert new : stagiaire + stage + dossier

--- pages/secretaire/create.php
+++ pages/secretaire/create.php
@@ -4,21 +4,33 @@ require("../../helpers/functions.php");
 
 if (isset($_POST["ok"])) {
 
-    $ID = generateID("S", "satage");
+    $newStage_id = generateID("T", "stage");
+    $newDossier_id = generateID("D", "dossier");
+    $newStagiaire_id = generateID("S", "stagiaire");
+    include("./file-proccess.php"); // Upload files
+
     $cin = $_POST["cin"];
     $nom = $_POST["nom"];
     $prenom = $_POST["prenom"];
     $sexe = $_POST["sexe"];
-    $mail = $_POST["mail"];
+    $email = $_POST["email"];
     $adresse = $_POST["adress"];
     $uv = $_POST["uv"];
     $tel = $_POST["tel"];
     $ville = $_POST["ville"];
     $niveau = $_POST["niveau"];
 
-    $req = "INSERT INTO `stagiaire` VALUES('$cin',' $nom','$prenom',' $sexe','$tel','$mail','$ville','$adresse','$uv','$niveau')";
-    $result = mysqli_query($con, $req);
-    if ($result) {
+    $stageReq = "INSERT INTO stage (ID_STAGE) VALUES ('$newStage_id');";
+    $dossierReq = "INSERT INTO dossier (ID_DOSSIER, CV, DEMANDE, ASSURANCE, DATE_DEPOSE, STATUT, ID_STAGE)
+            VALUES ('$newDossier_id', '$fileCv', '$fileDe', '$fileAs', NOW(), 'en attendant', '$newStage_id');"; //Skipped Col : PHOTO, COPY_CIN, AUTRE_FICHERS	
+    $stagiaireReq = "INSERT INTO stagiaire (ID_STAGIAIRE, CIN, NOM, PRENOM, SEXE, TEL, EMAIL, VILLE, ADRESSE, ETABLISSEMENT, NIVEAU, ID_STAGE)
+            VALUES('$newStagiaire_id', '$cin','$nom','$prenom','$sexe','$tel','$email','$ville','$adresse','$uv','$niveau', '$newStage_id');";
+
+    $stageReq_run = mysqli_query($con, $stageReq);
+    if ($stageReq_run) $dossierReq_run = mysqli_query($con, $dossierReq); // Check if stage created before create stagiaire
+    if ($dossierReq_run) $result = mysqli_query($con, $stagiaireReq); // Check if dossier created before create stagiaire
+
+    if (isset($result)) {
 
         $res = [
             'status' => 200,
